43183d4a1bdf6d859896b26b3f5cc7c129331f9d
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2023-03-16 17:47:13 -0300

    refactor: splits double ternary into a if else statement and extracts typeguard to a function

--- src/object-flat-list-item.tsx
+++ src/object-flat-list-item.tsx
@@ -32,14 +32,24 @@ type ObjectFlatListItemProps<DataType extends Record<string, unknown>> = FlatLis
   ObjectFlatListItemExtra<DataType>
 >;
 
+const isKeyData = <DataType extends Record<string, unknown>>(
+  data: ObjectFlatListItemData<DataType>,
+): data is KeyBasedObjectFlatListItemData<DataType> => {
+  return 'key' in data;
+};
+
 export const ObjectFlatListItem = <DataType extends Record<string, unknown>>({
   data,
   extra,
 }: ObjectFlatListItemProps<DataType>) => {
   if (extra.obj == null) return null;
-  const value = 'key' in data ? extra.obj[data.key] : data.getValue(extra.obj);
-  const formattedValue =
-    data.formatter != null ? data.formatter(value) : value != null ? String(value) : undefined;
+  const value = isKeyData(data) ? extra.obj[data.key] : data.getValue(extra.obj);
+  let formattedValue: string | null = null;
+  if (data.formatter) {
+    formattedValue = data.formatter(value);
+  } else {
+    formattedValue = value != null ? String(value) : null;
+  }
   return (
     <FlatListItem
       subtitleLeft={data.label}
