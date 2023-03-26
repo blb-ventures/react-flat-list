d56cce80cc2bd43212605b2fa59223c5aea2e3af
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2023-03-26 00:56:34 -0300

    lint: uses @blb-ventures/eslint-config to improve configuration reusability lint: fixes some lint issues

--- src/flat-list-item.tsx
+++ src/flat-list-item.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { FC, ReactNode } from 'react';
